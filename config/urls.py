d44495ddcd5729a9547a8385d5c1e36297f939cf
protasevich <dima_protasevich92@mail.ru>
2022-11-12 19:44:17 +0300

    lesson_2 Code style fix

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
 
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", RedirectView.as_view(url="mainapp/")),
